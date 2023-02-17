bee8077d2e5126f3cad2c50e15250821980d3f0d
Nurken Zhappargaly <zhnurken@gmail.com>
2023-02-17 14:46:24 +0600

    add React Class Component to Employees add form

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,6 +1,24 @@
 import "./employees-add-form.css"
+import { Component } from "react";
 
-const EmployeesAddForm = () => {
+class EmployeesAddForm extends Component {
+constructor (props) {
+    super(props)
+    this.state = {
+        name: '',
+        salary: ''
+    }
+}
+
+onValueChange=(e)=>{
+    this.setState({
+        [e.target.name]: e.target.value
+    })
+}
+
+  render(){
+    const {name, salary} = this.state;
+
     return(
         <div className="app-add-form">
             <h3>Добавьте нового сотрудника</h3>
@@ -8,16 +26,23 @@ const EmployeesAddForm = () => {
                 className="add-form d-flex">
                 <input type="text"
                     className="form-control new-post-label"
-                    placeholder="Как его зовут?" />
+                    placeholder="Как его зовут?"
+                    name="name"
+                    value={name}
+                    onChange={this.onValueChange} />
                 <input type="number"
                     className="form-control new-post-label"
-                    placeholder="Зарплата в $?" />
+                    placeholder="Зарплата в $?"
+                    name="salary"
+                    value={salary}
+                    onChange={this.onValueChange} />
 
                 <button type="submit"
                         className="btn btn-outline-light">Добавить</button>
             </form>
         </div>
     )
+  }
 }
 
 export default EmployeesAddForm;
